d963a4f370337518cd6bed80a29563d778808956
guoqiang <guoqiang@rivergame.net-sase>
2024-01-31 16:40:23 +0800

    Initial commit

---
+++ src/main/java/com/rivergame/service/ClientService.java
@@ -0,0 +1,42 @@
+package com.rivergame;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+/**
+ * @author： guoqiang@rivergame.net
+ * @date： 2024/1/31
+ * @description：
+ * @version: 1.0
+ */
+public class ClientService {
+    public static void main(String[] args) throws Exception {
+        ClientService clientService = new ClientService();
+        try {
+            //基于gRPC远程调用对应的方法
+            clientService.remoteCall("wuyu");
+        } finally {
+        }
+    }
+    /**
+     * 基于gRPC框架的使用步骤，进行远程调用
+     * @param name
+     */
+    public void remoteCall(String name) {
+//        HelloMessage.HelloRequest request = HelloMessage.HelloRequest.newBuilder().setName(name).build();
+//        HelloMessage.HelloResponse response;
+//        try {
+//            // 基于访问地址 创建通道
+//            Channel channel =  ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+//            // 利用通道 创建一个桩（Stub）对象
+//            HelloGrpc.HelloBlockingStub blockingStub = HelloGrpc.newBlockingStub(channel);
+//            //通过桩对象来调用远程方法
+//            response = blockingStub.sayHello(request);
+//        } catch (StatusRuntimeException e) {
+//            return;
+//        }
+//        System.out.println("client端远程调用sayHello()的结果为：\n\n" + response.getMessage());
+    }
+//}
+}
---
+++ src/main/java/com/rivergame/service/ServerService.java
@@ -0,0 +1,8 @@
+package com.rivergame.service;/**
+  * @author： guoqiang@rivergame.net
+  * @date： 2024/1/31
+  * @description： 
+  * @version: 1.0
+*/
+public class ServerService {
+}
